9ca68f7d84b818ad3b11ca50b1002b8a2e40ac32
Swathikrishna US <swathikrishnaus2015@gmail.com>
2024-01-02 04:55:08 +0530

    did some changes in home page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -76,58 +76,114 @@
 
 
 	/* ----------------------- MENU ---------------------- */
+	// $( 'body' ).on('click', '.nav-btn',  function( event ){
+	// 	$( event.currentTarget ).toggleClass('active');
+	// 	$('.nav-menu').toggleClass('active');
+	// 	$( 'body' ).toggleClass('no-scroll');
+	// 	return false;
+	// });
+
+	// $( window ).on('resize.myTemplate', function(){
+	// 	$('body')[ ( $(this).width() <= 767 ) ? 'addClass' : 'removeClass' ]('isMobile')
+	// }).trigger( 'resize.myTemplate' );
+
+	// $( 'body' ).on('click', '.dropdown>a', function( event){
+	// 	if( ! $('body.isMobile')[0] ){
+	// 		return;
+	// 	}
+
+	// 	var $thisLi = $( event.currentTarget ).parents( 'li' ),
+	// 		$thisLiActive = $thisLi.hasClass( 'dropdown-active' );
+
+	// 	$('.dropdown-active').removeClass('dropdown-active').children('ul').slideUp('slow');
+
+	// 	if( ! $thisLiActive ){
+	// 		$thisLi.addClass('dropdown-active');
+	// 		$thisLi.children('ul').slideDown('slow');
+	// 	}
+
+	// 	return false;
+	// });
+
+	// $( 'body' ).on( 'mouseenter', '.dropdown', function(event){
+	// 	if( $('body.isMobile')[0] ){
+	// 		return;
+	// 	}
+
+	// 	var menuItem = $( event.currentTarget );
+
+	// 	if( menuItem[0].timeOutMenu ){
+	// 		clearTimeout( menuItem[0].timeOutMenu );
+	// 	}
+
+	// 	menuItem.addClass('active');
+	// }).on( 'mouseleave', '.dropdown', function( event ){
+	// 	if( $('body.isMobile')[0] ){
+	// 		return;
+	// 	}
+
+	// 	var menuItem = $( event.currentTarget );
+
+	// 	menuItem[0].timeOutMenu = setTimeout( function(){
+	// 		menuItem.removeClass('active');
+	// 	}, 500 );
+	// });
+
+	/* ----------------------- MENU ---------------------- */
 	$( 'body' ).on('click', '.nav-btn',  function( event ){
-		$( event.currentTarget ).toggleClass('active');
-		$('.nav-menu').toggleClass('active');
-		$( 'body' ).toggleClass('no-scroll');
-		return false;
+	$( event.currentTarget ).toggleClass('active');
+	$('.nav-menu').toggleClass('active');
+	$( 'body' ).toggleClass('no-scroll');
+	return false;
 	});
 
 	$( window ).on('resize.myTemplate', function(){
-		$('body')[ ( $(this).width() <= 767 ) ? 'addClass' : 'removeClass' ]('isMobile')
+	$('body')[ ( $(this).width() <= 767 ) ? 'addClass' : 'removeClass' ]('isMobile')
 	}).trigger( 'resize.myTemplate' );
 
 	$( 'body' ).on('click', '.dropdown>a', function( event){
-		if( ! $('body.isMobile')[0] ){
-			return;
-		}
+	if( ! $('body.isMobile')[0] ){
+		return;
+	}
 
-		var $thisLi = $( event.currentTarget ).parents( 'li' ),
-			$thisLiActive = $thisLi.hasClass( 'dropdown-active' );
+	var $thisLi = $( event.currentTarget ).parents( 'li' ),
+		$thisLiActive = $thisLi.hasClass( 'dropdown-active' );
 
-		$('.dropdown-active').removeClass('dropdown-active').children('ul').slideUp('slow');
+	$('.dropdown-active').removeClass('dropdown-active').children('ul').slideUp('slow');
 
-		if( ! $thisLiActive ){
-			$thisLi.addClass('dropdown-active');
-			$thisLi.children('ul').slideDown('slow');
-		}
+	if( ! $thisLiActive ){
+		$thisLi.addClass('dropdown-active');
+		$thisLi.children('ul').slideDown('slow');
+	}
 
-		return false;
+	return false;
 	});
 
 	$( 'body' ).on( 'mouseenter', '.dropdown', function(event){
-		if( $('body.isMobile')[0] ){
-			return;
-		}
+	if( $('body.isMobile')[0] ){
+		return;
+	}
 
-		var menuItem = $( event.currentTarget );
+	var menuItem = $( event.currentTarget );
 
-		if( menuItem[0].timeOutMenu ){
-			clearTimeout( menuItem[0].timeOutMenu );
-		}
+	if( menuItem[0].timeOutMenu ){
+		clearTimeout( menuItem[0].timeOutMenu );
+	}
 
-		menuItem.addClass('active');
+	menuItem.addClass('active');
 	}).on( 'mouseleave', '.dropdown', function( event ){
-		if( $('body.isMobile')[0] ){
-			return;
-		}
+	if( $('body.isMobile')[0] ){
+		return;
+	}
 
-		var menuItem = $( event.currentTarget );
+	var menuItem = $( event.currentTarget );
 
-		menuItem[0].timeOutMenu = setTimeout( function(){
-			menuItem.removeClass('active');
-		}, 500 );
+	menuItem[0].timeOutMenu = setTimeout( function(){
+		menuItem.removeClass('active');
+	}, 500 );
 	});
+
+
 
 	/* ------------------- TO TOP ------------------ */
 	$(window).on( 'scroll.myTemplat', scrollWindow).trigger( 'scroll.myTemplat' );
