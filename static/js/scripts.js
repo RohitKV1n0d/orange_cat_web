42b6237d68fd4a8715cff4c796b066877f0300cc
Swathikrishna US <swathikrishnaus2015@gmail.com>
2024-01-17 14:45:58 +0530

    removd top bar header section, removed anchor line effect of active links, made the changes mentioned in issue #70

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -843,6 +843,50 @@ $(document).ready( function() {
 	
 
 
-// ----------video gallery---------------
+// ----------scroll smooth---------------
 
+
+const body = document.body,
+        jsScroll = document.getElementsByClassName('js-scroll')[0],
+        speed = 0.05;
+
+    let height, offset, lastScale;
+
+    function updateDimensions() {
+        height = jsScroll.getBoundingClientRect().height - window.innerHeight;
+        
+        if (height < 0) {
+        height = 0;
+        }
+        offset = 0;
+        body.style.height = Math.floor(height + window.innerHeight) + "px";
+    }
+
+    // Initial setup and update on window resize
+    updateDimensions();
+    window.addEventListener('resize', updateDimensions);
+
+    function checkZoom() {
+        const currentScale = window.innerHeight / window.innerHeight;
+        if (currentScale !== lastScale) {
+        updateDimensions();
+        lastScale = currentScale;
+    }
+
+      // Schedule the next check after 200 milliseconds
+        setTimeout(checkZoom, 200);
+    }
+
+    // Start the periodic check
+    checkZoom();
+
+    function smoothScroll() {
+        offset += (window.scrollY - offset) * speed;
+        offset = Math.max(0, Math.min(offset, height));
+        const scroll = `translateY(-${offset}px) translateZ(0)`;
+        jsScroll.style.transform = scroll;
+        requestAnimationFrame(smoothScroll);
+    }
+
+    smoothScroll();
 
