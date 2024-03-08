e0317ac7ed9f26bffbb8443c66d6017c37179335
RohitKV1n0d <rohitvinod92@gmail.com>
2024-03-08 10:01:24 +0530

    Refactor EmailUtils class and update send_email_task method

--- utils/EmailUtils.py
+++ utils/EmailUtils.py
@@ -1,29 +1,27 @@
-from celery import shared_task
-from flask import current_app
-from flask_mail import Mail, Message
+# from app import celery
+# from flask_mail import Mail, Message
+# import os
 
-class EmailUtils:
-    def __init__(self, app):
-        self.app = app
-        self.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-        self.app.config['MAIL_PORT'] = 587
-        self.app.config['MAIL_USE_TLS'] = True
-        self.app.config['MAIL_USERNAME'] = 'info@orangecatcycles.com'  # Your Google Workspace email
-        self.app.config['MAIL_PASSWORD'] = 'kkqz owcg sxgi hdsx'  # Your generated app password
-        self.mail = Mail(self.app)
+# class EmailUtils:
+#     def __init__(self, app):
+#         self.app = app
+#         self.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+#         self.app.config['MAIL_PORT'] = 587
+#         self.app.config['MAIL_USE_TLS'] = True
+#         self.app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'info@orangecatcycles.com')
+#         self.app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
+#         self.mail = Mail(self.app)
 
-    @shared_task(name='app.send_email', bind=True)
-    def sendMail(subject, body, recipient):
-        # try:
-        #     with current_app.app_context():
-        #         sender = 'info@orangecatcycles.com'
-        #         msg = Message(subject,
-        #                       sender=sender,
-        #                       recipients=[recipient])
-        #         msg.body = body
-        #         current_app.mail.send(msg)
-        #         return True
-        # except Exception as e:
-        #     print(e)
-        #     return False
-        pass
+#     @celery.task(name='app.send_email_task')
+#     def send_email_task(self, subject, sender, recipients, body):
+#         try:
+#             print('Sending email')
+#             msg = Message(subject, sender=sender, recipients=recipients)
+#             msg.body = body
+#             self.mail.send(msg)
+#             print('Email sent')
+#             return True
+#         except Exception as e:
+#             print(e)
+#             return False
+            
