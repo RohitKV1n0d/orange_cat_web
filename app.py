c1e60431205ab8746db4f7d10c5708b461654af4
Rohit Vinod <rohitvinod92@gmail.com>
2024-05-08 15:08:04 +0400

    Add login view to LoginManager #151

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ app.secret_key = os.environ.get('SECRET_KEY', 'your_secret_key')
 # Initialize Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'user_login'
 
 @celery.task(name='app.send_email_task')
 def send_email_task(subject, body, recipients):
